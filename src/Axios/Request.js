ac995cb73d117dce603fd4a422000c236b0f7fd2
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-08-01 19:18:36 +0530

    change

--- src/Axios/Request.js
+++ src/Axios/Request.js
@@ -1,2 +1,18 @@
 import axios from "axios";
 import { base_url } from "../BaseUrl/baseurl";
+
+const instance = axios.create({
+    baseURL: base_url,
+    timeout: 1000,
+  });
+
+export const sendrequest = (config) =>{
+return instance.request(config);
+}
+
+export const getdata = (path) =>{
+return sendrequest({
+    url: path,
+    method: 'GET',
+})
+}

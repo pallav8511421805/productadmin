5b66401e866c4b62303c9f4326167223edcbd1f5
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-08 16:51:10 +0530

    change

--- src/Redux/actions/product.actions.js
+++ src/Redux/actions/product.actions.js
@@ -1,141 +1,151 @@
-import { Addalldata, getalldata } from '../../Axios/APIS/Product.api';
-import { base_url } from '../../BaseUrl/baseurl';
-import * as Actiontypes from '../actions/Actiontype';
+import { Addalldata, getalldata } from '../../Axios/APIS/Product.api'
+import { base_url } from '../../BaseUrl/baseurl'
+import * as Actiontypes from '../actions/Actiontype'
 
 export const getproduct_data = () => (dispatch) => {
-    try {
-        dispatch(loaddata())
-        setTimeout(function () {
+  try {
+    dispatch(loaddata())
+    setTimeout(function () {
+      getalldata()
+        .then((data) =>
+          dispatch({ type: Actiontypes.getproductdata, payload: data.data }),
+        )
+        .catch((error) => dispatch(errordata(error.message)))
 
-            getalldata()
-                .then(data => dispatch(({ type: Actiontypes.getproductdata, payload: data.data })))
-                .catch(error => dispatch(errordata(error.message)));
+      // fetch(base_url + 'product')
+      //     .then(response => {
+      //         if (response.ok) {
+      //             return response;
+      //         } else {
+      //             var error = new Error('Error ' + response.status + ': ' + response.statusText);
+      //             error.response = response;
+      //             throw error;
+      //         }
+      //     },
+      //         error => {
+      //             var errmess = new Error(error.message);
+      //             throw errmess;
+      //         })
+      //     .then(response => response.json())
 
-            // fetch(base_url + 'product')
-            //     .then(response => {
-            //         if (response.ok) {
-            //             return response;
-            //         } else {
-            //             var error = new Error('Error ' + response.status + ': ' + response.statusText);
-            //             error.response = response;
-            //             throw error;
-            //         }
-            //     },
-            //         error => {
-            //             var errmess = new Error(error.message);
-            //             throw errmess;
-            //         })
-            //     .then(response => response.json())
-
-            // .then(data => dispatch(({ type: Actiontypes.getproductdata, payload:data.data})))
-            // .catch(error => dispatch(errordata(error.message)));
-        }, 2000)
-    } catch (error) {
-        dispatch(errordata(error.message))
-    }
+      // .then(data => dispatch(({ type: Actiontypes.getproductdata, payload:data.data})))
+      // .catch(error => dispatch(errordata(error.message)));
+    }, 2000)
+  } catch (error) {
+    dispatch(errordata(error.message))
+  }
 }
 
 export const Adddata = (data) => (dispatch) => {
-    try {
-        dispatch(loaddata())
-        setTimeout(function () {
-            Addalldata(data)
-                .then(data => dispatch(({ type: Actiontypes.Add_product, payload: data.data })))
-                .catch(error => dispatch(errordata(error.message)));
-            // fetch(base_url + 'product')
-            //     .then(response => {
-            //         if (response.ok) {
-            //             return response;
-            //         } else {
-            //             var error = new Error('Error ' + response.status + ': ' + response.statusText);
-            //             error.response = response;
-            //             throw error;
-            //         }
-            //     },
-            //         error => {
-            //             var errmess = new Error(error.message);
-            //             throw errmess;
-            //         })
-            // fetch(base_url + 'product', {
-            //     method: 'POST',
-            //     headers: {
-            //         'Content-Type': 'application/json',
-            //     },
-            //     body: JSON.stringify(data),
-            // })
-            //     .then(response => response.json())
-            //     .then(data => dispatch(({ type: Actiontypes.Add_product, payload: data.data})))
-            //     .catch(error => dispatch(errordata(error.message)));
-        }, 2000)
-    } catch (error) {
-        dispatch(errordata(error.message))
-    }
+  try {
+    dispatch(loaddata())
+    setTimeout(function () {
+      Addalldata(data)
+        .then((data) =>
+          dispatch({ type: Actiontypes.Add_product, payload: data.data }),
+        )
+        .catch((error) => dispatch(errordata(error.message)))
+      // fetch(base_url + 'product')
+      //     .then(response => {
+      //         if (response.ok) {
+      //             return response;
+      //         } else {
+      //             var error = new Error('Error ' + response.status + ': ' + response.statusText);
+      //             error.response = response;
+      //             throw error;
+      //         }
+      //     },
+      //         error => {
+      //             var errmess = new Error(error.message);
+      //             throw errmess;
+      //         })
+      // fetch(base_url + 'product', {
+      //     method: 'POST',
+      //     headers: {
+      //         'Content-Type': 'application/json',
+      //     },
+      //     body: JSON.stringify(data),
+      // })
+      //     .then(response => response.json())
+      //     .then(data => dispatch(({ type: Actiontypes.Add_product, payload: data.data})))
+      //     .catch(error => dispatch(errordata(error.message)));
+    }, 2000)
+  } catch (error) {
+    dispatch(errordata(error.message))
+  }
 }
 
 export const Editdata = (data) => (dispatch) => {
-    try {
-        fetch(base_url + 'Patients')
-            .then(response => {
-                if (response.ok) {
-                    return response;
-                } else {
-                    var error = new Error('Error ' + response.status + ': ' + response.statusText);
-                    error.response = response;
-                    throw error;
-                }
-            },
-                error => {
-                    var errmess = new Error(error.message);
-                    throw errmess;
-                })
-        fetch(base_url + 'product/' + data.id, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        })
-            .then(response => response.json())
-            .then(data => dispatch(({ type: Actiontypes.Edit_product, payload: data })))
-            .catch(error => dispatch(errordata(error.message)));
-
-    } catch (error) {
-        dispatch(errordata(error.message))
-    }
+  try {
+    fetch(base_url + 'product/').then(
+      (response) => {
+        if (response.ok) {
+          return response
+        } else {
+          var error = new Error(
+            'Error ' + response.status + ': ' + response.statusText,
+          )
+          error.response = response
+          throw error
+        }
+      },
+      (error) => {
+        var errmess = new Error(error.message)
+        throw errmess
+      },
+    )
+    fetch(base_url + 'product/' + data.id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) =>
+        dispatch({ type: Actiontypes.Edit_product, payload: data }),
+      )
+      .catch((error) => dispatch(errordata(error.message)))
+  } catch (error) {
+    dispatch(errordata(error.message))
+  }
 }
 
 export const Deletedata = (id) => (dispatch) => {
-    try {
-        fetch(base_url + 'product')
-            .then(response => {
-                if (response.ok) {
-                    return response;
-                } else {
-                    var error = new Error('Error ' + response.status + ': ' + response.statusText);
-                    error.response = response;
-                    throw error;
-                }
-            },
-                error => {
-                    var errmess = new Error(error.message);
-                    throw errmess;
-                })
-        fetch(base_url + 'product/' + id, {
-            method: 'DELETE',
-        })
-            .then(data => dispatch(({ type: Actiontypes.Delete_product, payload: data })))
-            .catch(error => dispatch(errordata(error.message)));
-
-
-    } catch (error) {
-        dispatch(errordata(error.message))
-    }
+  try {
+    fetch(base_url + 'product').then(
+      (response) => {
+        if (response.ok) {
+          return response
+        } else {
+          var error = new Error(
+            'Error ' + response.status + ': ' + response.statusText,
+          )
+          error.response = response
+          throw error
+        }
+      },
+      (error) => {
+        var errmess = new Error(error.message)
+        throw errmess
+      },
+    )
+    fetch(base_url + 'product/' + id, {
+      method: 'DELETE',
+    })
+      .then((data) =>
+        dispatch({ type: Actiontypes.Delete_product, payload: data }),
+      )
+      .catch((error) => dispatch(errordata(error.message)))
+  } catch (error) {
+    dispatch(errordata(error.message))
+  }
 }
 
 export const loaddata = () => (dispatch) => {
-    dispatch({ type: Actiontypes.Load_Product })
+  dispatch({ type: Actiontypes.Load_Product })
 }
 
 export const errordata = (error) => (dispatch) => {
-    dispatch({ type: Actiontypes.Error_Product, payload: error })
+  dispatch({ type: Actiontypes.Error_Product, payload: error })
 }
